1dad11702b04336828a2169608c527573053e53b
theoldestnoob <will.hinz@gmail.com>
2019-06-18 20:51:27 -0500

    initial commit, hello world

---
+++ engine.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 20:28:25 2019
+
+@author: willh
+"""
+
+import tcod as libtcod
+
+
+def main():
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
