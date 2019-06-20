337485a154f6344a7c1587e2b00049f994b7bcf3
theoldestnoob <will.hinz@gmail.com>
2019-06-19 19:44:35 -0500

    rewrote input handler class to use keymap dict for key input removed unnecessary functions from input handler module

--- input_handlers.py
+++ input_handlers.py
@@ -8,44 +8,6 @@ Created on Tue Jun 18 21:35:02 2019
 import tcod.event
 
 
-def handle_input(event):
-    if event.type == "KEYDOWN":
-        return handle_keys(event)
-    elif event.type == "QUIT":
-        return handle_quit(event)
-
-    return {}
-
-
-def handle_keys(event):
-    # debug output
-    # print(event)
-
-    # movement keys
-    if event.sym == tcod.event.K_UP:
-        return {"move": (0, -1)}
-    elif event.sym == tcod.event.K_DOWN:
-        return {"move": (0, 1)}
-    elif event.sym == tcod.event.K_LEFT:
-        return {"move": (-1, 0)}
-    elif event.sym == tcod.event.K_RIGHT:
-        return {"move": (1, 0)}
-
-    # fullscreen
-    if event.sym == tcod.event.K_RETURN and event.mod & tcod.event.KMOD_LALT:
-        return {"fullscreen": True}
-
-    # exit the game
-    if event.sym == tcod.event.K_ESCAPE:
-        return {"exit": True}
-
-    return {}
-
-
-def handle_quit(event):
-    return {"exit": True}
-
-
 class InputHandler(tcod.event.EventDispatch):
 
     def __init__(self):
@@ -55,24 +17,13 @@ class InputHandler(tcod.event.EventDispatch):
         self._actionq.append({"exit": True})
 
     def ev_keydown(self, event):
-        # movement keys
-        if event.sym == tcod.event.K_UP:
-            self._actionq.append({"move": (0, -1)})
-        elif event.sym == tcod.event.K_DOWN:
-            self._actionq.append({"move": (0, 1)})
-        elif event.sym == tcod.event.K_LEFT:
-            self._actionq.append({"move": (-1, 0)})
-        elif event.sym == tcod.event.K_RIGHT:
-            self._actionq.append({"move": (1, 0)})
-
-        # fullscreen
+        # first check for combination keys
         if (event.sym == tcod.event.K_RETURN
                 and event.mod & tcod.event.KMOD_LALT):
             self._actionq.append({"fullscreen": True})
-
-        # exit the game
-        if event.sym == tcod.event.K_ESCAPE:
-            self._actionq.append({"exit": True})
+        # if not combination, push the keymapped action to our action queue
+        elif event.sym in input_keymap.keys():
+            self._actionq.append(input_keymap[event.sym])
 
     def get_action(self):
         if self._actionq:
@@ -82,3 +33,12 @@ class InputHandler(tcod.event.EventDispatch):
 
     def clear_actionq(self):
         self._actionq.clear()
+
+
+input_keymap = {
+        tcod.event.K_UP: {"move": (0, -1)},
+        tcod.event.K_DOWN: {"move": (0, 1)},
+        tcod.event.K_LEFT: {"move": (-1, 0)},
+        tcod.event.K_RIGHT: {"move": (1, 0)},
+        tcod.event.K_ESCAPE: {"exit": True}
+        }
