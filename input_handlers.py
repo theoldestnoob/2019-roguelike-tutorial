64141c4a91cec813ad73a6d3627ddb9797989a8f
theoldestnoob <will.hinz@gmail.com>
2019-06-19 20:23:00 -0500

    added dict for lalt-modified keys to reduce future if/elif nonsense

--- input_handlers.py
+++ input_handlers.py
@@ -17,13 +17,13 @@ class InputHandler(tcod.event.EventDispatch):
         self._actionq.append({"exit": True})
 
     def ev_keydown(self, event):
-        # first check for combination keys
-        if (event.sym == tcod.event.K_RETURN
-                and event.mod & tcod.event.KMOD_LALT):
-            self._actionq.append({"fullscreen": True})
-        # if not combination, push the keymapped action to our action queue
-        elif event.sym in input_keymap.keys():
-            self._actionq.append(input_keymap[event.sym])
+        # first check for mapped modified keys
+        if (event.mod & tcod.event.KMOD_LALT
+                and event.sym in in_keymap_lalt.keys()):
+            self._actionq.append(in_keymap_lalt[event.sym])
+        # if no meaningful modified keys, push the mapped action to our queue
+        elif event.sym in in_keymap_nomod.keys():
+            self._actionq.append(in_keymap_nomod[event.sym])
 
     def get_action(self):
         if self._actionq:
@@ -35,10 +35,14 @@ class InputHandler(tcod.event.EventDispatch):
         self._actionq.clear()
 
 
-input_keymap = {
+in_keymap_nomod = {
         tcod.event.K_UP: {"move": (0, -1)},
         tcod.event.K_DOWN: {"move": (0, 1)},
         tcod.event.K_LEFT: {"move": (-1, 0)},
         tcod.event.K_RIGHT: {"move": (1, 0)},
         tcod.event.K_ESCAPE: {"exit": True}
+        }
+
+in_keymap_lalt = {
+        tcod.event.K_RETURN: {"fullscreen": True}
         }
